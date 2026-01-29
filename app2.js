b9bc1c38ef36691bc4c1edb12246b23858f3acca
Silanda <silanda@aluno.unilab.edu.br>
2026-01-29 11:25:24 -0300

    Implement sign-out and member registration features
    Added sign-out functionality and member registration validation.

--- app2.js
+++ app2.js
@@ -1,5 +1,12 @@
+// Working with item bars
 const profile = document.getElementById("profile");
 const members = document.getElementById("members");
+const signOut = document.getElementById("signOut");
+
+signOut.addEventListener("click", () =>{
+    alert("You have sign out from the page!")
+    window.location = "http://127.0.0.1:5500/index0.html"
+})
 
 profile.addEventListener("click", () =>{
     const regist = document.getElementById("regist");
@@ -16,4 +23,62 @@ members.addEventListener("click", () =>{
 });
 close2.addEventListener("click", () =>{
     dat.style.display = "none";
-})
+});
+
+// Working with register enter
+let btnAddingMembers = document.getElementById("add");
+let membersData = [];
+btnAddingMembers.addEventListener("click", () =>{
+    let memberName = document.getElementById("name").value;
+    let memberAge = document.getElementById("age").value;
+    let sexM = document.getElementById("sexM").checked;
+    let sexF = document.getElementById("sexF").checked;
+    let sex = document.getElementById("sex");
+    
+    switch(true){
+        case memberName == "" || !isNaN(memberName):
+            alert("Please, insert correctly new member name!");
+            break;
+
+        case memberAge == "" || isNaN(memberAge):
+            alert("Please, insert correctly new member age!");
+            break;
+        default:
+            alert("All new member information was sent.");
+            break;
+    }
+    
+    // Working with informations's buttons
+    let btnS = document.getElementById("btnS1");
+    btnS.addEventListener("click", () =>{
+        let name1 = document.getElementById("name1");
+        membersData.push({
+        memberName: memberName,
+        memberAge: memberAge,
+        sex: sex
+        });
+    
+        // Working with new members descriptions
+        let joinData = "";
+        for(let s = 0; s < membersData.length; s++){
+            joinData += (s + 1) + " - " + "Name: " + membersData[s].memberName + ";" + " Age: " + membersData[s].memberAge + ";" + "\n\n";
+        }
+        if(!sexF.checked){
+            sex = "male";
+            name1.textContent = joinData + " Gender: " + sex;
+        }
+        if(!sexM.checked){
+            sex = "female";
+            name1.textContent = joinData + " Gender: " + sex;
+        }
+    });
+
+    // Let fill field empty after register new member;
+
+    document.getElementById("name").value = " ";
+    document.getElementById("age").value = " ";
+    document.getElementById("sexF").checked = "";
+    document.getElementById("sexM").checked = "";
+    memberName.focus();
+    return;
+});
